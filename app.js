57eee1ca33342b056f937e39de952f7998e65d45
Milán Nagy <123.wizek@gmail.com>
2013-09-24 11:41:56 +0200

    Added Ko.js, begun writing viewmodel.

--- app.js
+++ app.js
@@ -3,33 +3,49 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express')
+var http = require('http')
+var path = require('path')
 
-var app = express();
+var app = express()
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.bodyParser())
+app.use(express.methodOverride())
+app.use(app.router)
+app.use(express.static(path.join(__dirname, 'public')))
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(express.errorHandler())
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' })
+})
+
+app.get('/api/publishing', function(req, res) {
+  res.send({a:1})
+})
+app.get('/api/reach', function(req, res) {
+  res.send({a:1})
+})
+
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+  console.log('Express server listening on port ' + app.get('port'))
+})
+
+require('jade').filters.loadr = function(content, filename) {
+  var middle = content.replace(/[\s\n,]/g, ',')
+  var full = ''
+    + '<script src="http://loadr.aws.af.cm/load?packages='
+    + middle
+    + '"></script>'
+  return full
+}
